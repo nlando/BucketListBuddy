b9ddf565371d548b31e68535fd8539fdadb0fb0a
Nick Landon <nicklandon.co@gmail.com>
2021-07-24 12:47:55 -0700

    Create trips/index HTML

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const app = express();
 //Sets views to ejs
 app.set('view engine', 'ejs');
 
+//Pulls in index route
+const indexRouter = require('./routes/index');
+
+//Pulls in trips router
+const tripsRouter = require('./routes/trips');
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,17 +25,15 @@ app.use((req, res, next) => {
     next();
 });
 
-
-
 //Server uses indexRouter at url of /
 app.use('/', indexRouter);
 
 //Server uses tripsRouter at url of /trips
 app.use('/trips', tripsRouter);
 
-const PORT = 4000;
-
 //Server listen at localhost://port
+const port = 4000;
+
 app.listen(port, () => {
     console.log(`reporting live from localhost://${port}`)
 })
