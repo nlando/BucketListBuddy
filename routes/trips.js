5694460c5a81339b84dd12c8d26d0aa75a4f4887
Nick Landon <nicklandon.co@gmail.com>
2021-07-24 12:10:18 -0700

    Create trips router

--- routes/trips.js
+++ routes/trips.js
@@ -2,24 +2,25 @@
 
 //Pulls in express
 const express = require('express');
+const trips = require('../controllers/trips');
 
 //Creates a variable for expresses router object
 const router = express.Router();
 
-// Pulls in all the functions from flights controller
-const bucketListsController = require('../controllers/bucketLists');
+// Pulls in all the functions from trips controller
+const tripsController = require('../controllers/trips');
 
-//On / URL, gets/calls index function from flights controller, which renders all the found flights on the URL of flights/
-router.get('/', bucketListsController.index);
+//On / URL, gets/calls index function from trips controller, which renders all the found flights on the URL of flights/
+router.get('/', tripsController.index);
 
-// //On /new URL, gets/calls new function from flights controller, which renders flights/new with the title of Add Flight
-// router.get('/new', bucketListsController.new);
+// //On /new URL, gets/calls 'new' function from trips controller, which renders trips/new with the title of Add Bucket List Trip
+router.get('/new', tripsController.new);
 
-// //On /:id URL, gets/calls show function from flights controller, which renders flights/show with the found flight --DETIALS PAGE--
-// router.get('/:id', bucketListsController.details);
+// //On /:id URL, gets/calls details function from trips controller, which renders trips/details with the found trip --DETAILS PAGE--
+router.get('/:id', tripsController.details);
 
-// //On / URL, gets/calls create function from the flights controller, which creates and saves from the flight model
-// router.post('/', bucketListsController.create);
+// //On / URL, gets/calls create function from the trips controller, which creates and saves from the bucketlist-trip model
+router.post('/', tripsController.create);
 
 //exports all the routers from this page
 module.exports = router;
