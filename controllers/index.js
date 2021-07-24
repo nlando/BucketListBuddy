d3820ebeafd7e9eebcf5153132c21e0827824b90
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2021-07-24 13:56:25 -0600

    update index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
 //CONTROLLER INDEX
 
 //calling index renders the index (home) page
-function index(req, res){
-    res.render('index', {
-        user: req.user
-    });
-}
+function index(req, res, next) {
+    User.find({}, function(err, users) {
+     res.render('user/index', {
+      users,
+      user: req.user
+      });
+   });
+  }
 
 //Exports index function
 module.exports = {
